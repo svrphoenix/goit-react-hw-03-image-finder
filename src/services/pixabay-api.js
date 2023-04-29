08cec642ad65bed51fc11ea8128488d4446a7c87
Serhii Rudenko <svr.phoenix@gmail.com>
2023-04-29 16:14:45 +0200

    Changed API service

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const API_KEY = '34471861-e8084534cfe7ae4300b245128';
+const BASE_URL = 'https://pixabay.com/api/';
+
 export const ITEMS_PER_PAGE = 12;
 
-axios.defaults.baseURL = 'https://pixabay.com';
+// axios.defaults.baseURL = 'https://pixabay.com';
 
 export const readPixabayImages = async (q, page) => {
   const options = {
@@ -17,6 +19,6 @@ export const readPixabayImages = async (q, page) => {
       per_page: ITEMS_PER_PAGE,
     },
   };
-  const { data } = await axios.get('/api', options);
+  const { data } = await axios.get(BASE_URL, options);
   return data;
 };
